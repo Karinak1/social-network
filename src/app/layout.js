d0a9354b1c418f88563fe1e36771096ffe81f0ed
volto9 <volto9@yahoo.com>
2024-12-10 13:04:42 +0000

    neww

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
             <Link href="/">Home</Link>
             <Link href="/posts">Posts</Link>
             <Link href="/user">User</Link>
+            <Link href="/details">Details</Link>
           </nav>
           <SignedOut>
             <SignInButton />
